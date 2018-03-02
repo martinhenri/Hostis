d18595446965c2ab4f26ee4c1eeab65d57fd9089
Unknown <martin.henri09@gmail.com>
2018-03-02 10:19:50 +0100

    Ajout des interfaces de jojo

--- src/user_interface/Acceuil_SecretaireMed.java
+++ src/user_interface/Acceuil_SecretaireMed.java
@@ -226,9 +226,6 @@ public class Acceuil_SecretaireMed extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonDeconnexionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonDeconnexionActionPerformed
-        Identification id = new Identification();
-        id.setVisible(true);
-        this.dispose();
     }//GEN-LAST:event_jButtonDeconnexionActionPerformed
 
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
